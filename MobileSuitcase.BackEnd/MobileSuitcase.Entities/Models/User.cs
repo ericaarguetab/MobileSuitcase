f6edfb60e5159531bfbf9a0d2bdceddfc9d4d980
Erica Argueta <ericaarguetab@gmail.com>
2019-04-24 17:11:56 -0600

    Creating models

---
+++ MobileSuitcase.BackEnd/MobileSuitcase.Entities/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MobileSuitcase.Entities
+{
+    public class User
+    {
+        public int UserId { get; set; }
+
+        [Required(ErrorMessage = "El nombre de usuario es requerido")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "El correo electrónico es requerido")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "La contraseña es requerida")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Campo nombre es requerido")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Campo apellido es requerido")]
+        public string LastName { get; set; }
+    }
+}
---
+++ MobileSuitcase.BackEnd/MobileSuitcase.Entities/Models/Worker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MobileSuitcase.Entities.Models
+{
+    class Worker
+    {  
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string ImagePath { get; set; }
+
+        public string Position { get; set; }
+
+        public int Age { get; set; }
+
+    }
+}
